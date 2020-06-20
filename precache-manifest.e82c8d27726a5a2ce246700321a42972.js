53ce6ed28ae09bb5f305f44c6bcdb371ebc8b2bf
Sean Baklini <sbaklini@unm.edu>
2020-06-20 16:06:20 -0600

    Updates

---
+++ precache-manifest.e82c8d27726a5a2ce246700321a42972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fcf30f226f892d9930de26b62cc6fc",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b7ab850007a5061c9858",
+    "url": "/clicky-game/static/css/main.3e87508b.chunk.css"
+  },
+  {
+    "revision": "6ce6f54f724085dc1c01",
+    "url": "/clicky-game/static/js/2.93eb1781.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.93eb1781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ab850007a5061c9858",
+    "url": "/clicky-game/static/js/main.d09a4f39.chunk.js"
+  },
+  {
+    "revision": "ea8de61c71fd36f495fc",
+    "url": "/clicky-game/static/js/runtime-main.596060ab.js"
+  }
+]);
